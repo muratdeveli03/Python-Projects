a367790205bb13904a62d930eaa75b3cf1a517e2
Murat Develi <muratdeveli03@gmail.com>
2023-06-19 22:38:58 +0300

    1

--- csv_to_xlsx.py
+++ csv_to_xlsx.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the CSV file into a DataFrame
-data = pd.read_csv('input.csv')
+data = pd.read_csv('records.csv')
 
 # Convert the DataFrame to XLSX
 data.to_excel('output.xlsx', index=False)
